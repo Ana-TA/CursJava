68e5cb732e9ff2d7b2c3f0a68fffd2eb57b62e33
unknown <anna.anass@gmail.com>
2023-09-13 16:27:57 +0300

    Tema3

---
+++ src/curs3/Tema3.java
@@ -0,0 +1,27 @@
+package curs3;
+
+import java.util.Scanner;
+
+public class Tema3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ce varsta ai?");
+		Scanner scan = new Scanner(System.in);
+		int inputVarsta = scan.nextInt();
+		
+		if (inputVarsta < 18) {
+			System.out.println("Esti minor!");
+		}
+		else {
+			if ((inputVarsta) >= 18 && (inputVarsta <= 65)) {
+				System.out.println("Esti Adult!");
+			}
+			else {
+				System.out.println("Esti batran!");
+			}
+		}
+
+	}
+
+}
