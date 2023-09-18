5be7a7f4d4a2084a379bdb400b6f99f1bd926804
unknown <anna.anass@gmail.com>
2023-09-18 20:33:03 +0300

    curs5 commit2

--- src/curs5/DataTypes.java
+++ src/curs5/DataTypes.java
@@ -1,9 +1,16 @@
 package curs5;
 
 public class DataTypes {
-
+	
+	public short nrMeuShort;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		DataTypes obj = new DataTypes();
+		byte nrMeuByte = 127;
+		short nrMeuShort =  32000;
+		int nrMeuInteger = 2000000000;
+		System.out.println(nrMeuShort);
 
 	}
 
